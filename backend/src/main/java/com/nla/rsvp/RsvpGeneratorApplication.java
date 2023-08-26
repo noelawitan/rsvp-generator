31f3ade04c630e0adbd300cc1aceb07561b7b67e
noelawitan <noel.awitan@gmail.com>
2023-08-26 20:41:26 +0800

    Fixed the CORS issue when requesting to diff endpoints.

--- backend/src/main/java/com/nla/rsvp/RsvpGeneratorApplication.java
+++ backend/src/main/java/com/nla/rsvp/RsvpGeneratorApplication.java
@@ -1,13 +1,30 @@
 package com.nla.rsvp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RsvpGeneratorApplication {
+
+	@Value("${cors.allowed.origins}")
+	private String corsAllowedOrigins;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RsvpGeneratorApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins(corsAllowedOrigins);
+			}
+		};
+	}
 }
--- backend/src/main/java/com/nla/rsvp/config/SecurityConfig.java
+++ backend/src/main/java/com/nla/rsvp/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.nla.rsvp.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -10,13 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -27,21 +19,13 @@ public class SecurityConfig {
 
     private final AuthenticationProvider authenticationProvider;
 
-    @Value("{cors.allowed.origins")
-    private String allowedOrigins;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-
-
-        // Apply the CORS configuration to the HttpSecurity object
-        httpSecurity.cors().configurationSource(corsConfigurationSource())
-                // Existing configuration
-                .and()
+        httpSecurity
                 .csrf()
                 .disable()
                 .authorizeRequests()
-                .requestMatchers(new AntPathRequestMatcher("/auth/**"))
+                .antMatchers("/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
@@ -51,19 +35,6 @@ public class SecurityConfig {
                 .and()
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-
         return httpSecurity.build();
     }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("*"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-
 }
